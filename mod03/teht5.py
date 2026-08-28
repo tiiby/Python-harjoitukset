419165c9167c81fff5465e8e8dd7ceee28c5797c
tiiby <tiia.byman@gmail.com>
2026-08-28 09:34:42 +0300

    Peruttu vahingossa tallennetut muutokset tehtävään 5

--- mod03/teht5.py
+++ mod03/teht5.py
@@ -2,13 +2,15 @@ leiviskät_str = input("Anna leiviskät: ")
 naulat_str = input("Anna naulat: ")
 luodit_str = input("Anna luodit: ")
 
-leiviskät = float(leiviskät_str)
-naulat = float(naulat_str)
-luodit = float(luodit_str)
+luodit = luodit_str * 13,3
+naulat = naulat_str * 425,6
+leiviskät = leiviskät_str * 8512
 
-yhteensä_grammaa = (leiviskät * 8512) + (naulat * 425,6) + (luodit * 13,3)
+yhteensä_grammoina = leiviskät + naulat + luodit
 
-kilogrammat = int(yhteensä_grammaa // 1000)
-grammat = float(yhteensä_grammaa % 1000)
+
+kilogrammat = int(yhteensä_grammoina // 1000)
+grammat = float(yhteensä_grammoina % 1000)
+
 
 print("Massa nykymittojen mukaan: " + str(kilogrammat) + "kilogrammaa ja " + str(grammat) + " grammaa.")
