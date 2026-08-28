fcf10427bcb3f71a7efd0d5c0ac2cbe0068462f2
tiiby <tiia.byman@gmail.com>
2026-08-28 09:12:24 +0300

    Uusi yritys

--- tuntitehtävät/tuntienharjoitukset.py
+++ tuntitehtävät/tuntienharjoitukset.py
@@ -1,5 +1,7 @@
-kerrat = int(input("Montako kertaa tervehditään: "))
-tehdyt = 0
-while tehdyt < kerrat:
-    print("Hyvää huomenta")
-    tehdyt = tehdyt + 1
+import random
+noppa1 = noppa2 = heitot = 0
+while (noppa1 != 6 or noppa2 != 6):
+    noppa1 = random.randint(1,6)
+    noppa2 = random.randint(1,6)
+    heitot = heitot + 1
+print(f"Tarvittiin {heitot:d} heittoa.")
